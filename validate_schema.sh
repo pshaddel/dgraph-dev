cf954d555958d05101e6072fadaa954c22044f12
Poorshad <p.shaddel@gmail.com>
2023-03-12 17:12:42 +0100

    run the ci with bash

--- validate_schema.sh
+++ validate_schema.sh
@@ -1,4 +1,6 @@
-check=$(curl -X POST localhost:8080/admin/schema --data-binary "@schema.graphql");
+#!/bin/bash
+
+check=$(curl -X POST http://127.0.0.1:8080/admin/schema --data-binary "@schema.graphql");
 echo $check;
 # check if string "error" is in the response
 if grep -q "error" <<< "$check" ; then exit 1 ; fi
