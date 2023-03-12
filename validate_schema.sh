bf93f51ba3d37847620c3aecfe2c499100172e70
Poorshad <p.shaddel@gmail.com>
2023-03-12 17:17:50 +0100

    better schema validation condition / sleep until containers are ready

--- validate_schema.sh
+++ validate_schema.sh
@@ -3,5 +3,5 @@
 check=$(curl -X POST http://127.0.0.1:8080/admin/schema --data-binary "@schema.graphql");
 echo $check;
 # check if string "error" is in the response
-if grep -q "error" <<< "$check" ; then exit 1 ; fi
-exit 0
+if grep -q "Success" <<< "$check" ; then exit 0 ; fi
+exit 1
