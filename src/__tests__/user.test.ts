05f9e033d2c2b0eaaa48f05083c02259349d4f20
Poorshad <p.shaddel@gmail.com>
2023-03-15 22:30:57 +0100

    test: :test_tube: name the test better and avoid random number in the test

--- src/__tests__/user.test.ts
+++ src/__tests__/user.test.ts
@@ -2,10 +2,9 @@ import assert from "node:assert";
 import { query } from "./graphqlClient";
 describe("getUsers", () => {
   it("should return a list of users", async () => {
-    const rnd = Math.floor(Math.random() * 5);
     const { data, errors } = await query({
       functionName: "queryUser",
-      query: `query{queryUser(first: ${rnd}) {name}}`
+      query: `query{queryUser {name}}`
     });
     assert.strictEqual(errors, undefined);
     assert.strictEqual(typeof data, "object");
